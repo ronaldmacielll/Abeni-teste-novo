951a978ba6ddda9f9c742b250e637b3e278bccd1
Ronald de Souza Maciel <ronald_souzatqr@hotmail.com>
2026-05-02 14:25:24 -0300

    fix: remove unused Metadata import and update ESLint config

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { AuthProvider } from '@/modules/shared/hooks/useAuth'
